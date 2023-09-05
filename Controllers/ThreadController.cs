6e1a722eeac88d4f52969702f3448ac0d4735e98
45an <muse.yusuf@hotmail.com>
2023-09-05 10:06:19 +0200

    Added threadcontrollers

---
+++ Controllers/ThreadController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _4chanForum.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace _4chanForum.Controllers
+{
+    public class ThreadController : Controller
+    {
+
+        private readonly DataContext _context;
+
+        public ThreadController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index(int id)
+        {
+            
+            return View();
+        }
+
+        public IActionResult Thredas()
+        {
+            var threads = _context.Topics.ToList();
+            return View(threads);
+        }
+
+        public IActionResult Create ()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
+
--- Models/ThreadModel.cs
+++ Models/ThreadModel.cs
@@ -3,8 +3,8 @@
     public class ThreadModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Text { get; set; }
+        public string? Title { get; set; }
+        public string? Text { get; set; }
         public int TopicId { get; set; }
     }
 }
